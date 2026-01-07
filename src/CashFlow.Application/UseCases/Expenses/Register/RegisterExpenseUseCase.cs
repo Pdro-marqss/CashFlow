7a1b623be96dddce59e7f507d98509f8f6e56ae1
Pedro Marques <pdro.marqs2015@hotmail.com>
2026-01-07 00:07:29 -0300

    [feat] Criando função auxiliar para validar dados do requestRegisterExpense no useCase de registro de despesa

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,6 +8,36 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
+        Validate(request);
+
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if(titleIsEmpty)
+        {
+            throw new ArgumentException("Title value is required.");
+        }
+
+        var amountIsZeroOrNegative = request.Amount <= 0;
+        if(amountIsZeroOrNegative)
+        {
+            throw new ArgumentException("Amount must be greater than zero.");
+        }
+
+        var dateCompareResult = DateTime.Compare(request.Date, DateTime.UtcNow);
+        var dateIsInFuture = dateCompareResult > 0;
+        if (dateIsInFuture)
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if(paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Payment Type is not valid.");
+        }
     }
 }
