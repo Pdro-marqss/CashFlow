3b11104f7a2e529a5b743f9f07a96d17c202bdc8
Pedro Marques <pdro.marqs2015@hotmail.com>
2026-01-08 23:35:33 -0300

    [feat] Instalando FluentValidation e mudando as validaçoes da criação de despesas do modelo de ifs para a lib

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,4 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -15,29 +14,8 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if(titleIsEmpty)
-        {
-            throw new ArgumentException("Title value is required.");
-        }
+        var validator = new RegisterExpenseValidator();
 
-        var amountIsZeroOrNegative = request.Amount <= 0;
-        if(amountIsZeroOrNegative)
-        {
-            throw new ArgumentException("Amount must be greater than zero.");
-        }
-
-        var dateCompareResult = DateTime.Compare(request.Date, DateTime.UtcNow);
-        var dateIsInFuture = dateCompareResult > 0;
-        if (dateIsInFuture)
-        {
-            throw new ArgumentException("Expenses cannot be for the future.");
-        }
-
-        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-        if(paymentTypeIsValid == false)
-        {
-            throw new ArgumentException("Payment Type is not valid.");
-        }
+        var result = validator.Validate(request);
     }
 }
