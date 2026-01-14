532def65c45bfc238ad45d1442a87711e6cd0208
Pedro Marques <pdro.marqs2015@hotmail.com>
2026-01-13 23:19:45 -0300

    [feat] Configurando filtro no program.cs e atualizando codigo para usa-lo

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,7 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
-using CashFlow.Communication.Responses;
-using CashFlow.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -13,26 +11,11 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        try
-        {
-            var useCase = new RegisterExpenseUseCase();
+        var useCase = new RegisterExpenseUseCase();
 
-            var response = useCase.Execute(request);
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        } 
-        catch(ErrorOnValidationException ex)
-        {
-            ResponseErrorJson errorResponse = new(ex.Errors);
-
-            return BadRequest(errorResponse);
-        }
-        catch
-        {
-            ResponseErrorJson errorResponse = new("Unknown error");
-
-            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-        }
+        return Created(string.Empty, response);
     }
 }
 
--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,10 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
+using CashFlow.Api.Filters;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 var app = builder.Build();
 
